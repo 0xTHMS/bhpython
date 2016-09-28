1cd103edc42aec3d8c9941f4d92eb771e0fc960d
thms <you@example.com>
2016-09-28 23:20:14 +0200

    moved filed

---
+++ tcpserver.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+bind_ip = "0.0.0.0"
+bind_port = 1337
+
+# server object
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind to port
+server.bind((bind_ip, bind_port))
+server.listen(5)
+
+# accept
+print "Listening on %s port %s" % (bind_ip, bind_port)
+
+
+def handle_client(client_socket):
+    # print what client sends.
+    request = client_socket.recv(1024)
+    print "Received %s " % request
+
+    client_socket.send("ACK!")
+    client_socket.close()
+
+while True:
+    (client,addr) = server.accept()
+    print "Accepted connection from %s on port %s" % (addr[0],addr[1])
+
+    # thread for out client
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
+
+
+
