40f2471e6704731a5a4841b352d347e0c19680c6
thms <thms@localhost>
2016-10-26 16:27:40 +0200

    Raw sockets and raw TCP in p_sniffer (yes I know scapy)

--- p_sniffer.py
+++ p_sniffer.py
@@ -1,10 +1,50 @@
 import socket
 import os
+import struct
+from ctypes import *
+
 
 # host to listen on
-host = "127.0.0.1"
+host = "10.23.23.99"
+
+"""
+Our ip header
+"""
+class IP(Structure):
+    _fields_ = [
+        ("ihl" , c_ubyte, 4),
+        ("version", c_ubyte, 4),
+        ("tos", c_ubyte),
+        ("len", c_ushort),
+        ("id", c_suhort),
+        ("offset", c_ushort),
+        ("ttl", c_ubyte),
+        ("protocol_num", c_ubyte),
+        ("sum", c_ushort),
+        ("src", c_ulong),
+        ("dst", c_ulong)
+    ]
+
+    def __new__(self, socket_buffer=None):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+        # Map protocol constant to theur names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17:"UDP"}
+
+        # Human readble IP adress
+        self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+
+        # Human readable protocol
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
+
 
 # create a raw socket
+# os.name == posix on Linux, nt on windows.
 if os.name == "nt":
     socket_protocol = socket.IPPROTO_IP
 else:
@@ -21,9 +61,20 @@ sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 if os.name == "nt":
     sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-# read a single packet
-print sniffer.recvfrom(65565)
+try:
+    while True:
+        # read a single packet
+        raw_buffer = sniffer.recvfrom(65565)[0]
 
-# if windows, turn promisc off
-if os.name == "nt":
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        # create an IP header from the first 20 bytes of the buffer
+        ip_header = IP(raw_buffer[0:20])
+        print "Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
+
+# handle CTRL-C
+except KeyboardInterrupt:
+    # if windows, turn promisc off
+    if os.name == "nt":
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+
+
