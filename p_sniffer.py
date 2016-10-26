c6550a1edce1d5a178cdd97a2cd0436b7883cbe8
thms <thms@localhost>
2016-10-26 16:12:45 +0200

    first commit of rthe day

--- p_sniffer.py
+++ p_sniffer.py
@@ -5,3 +5,25 @@ import os
 host = "127.0.0.1"
 
 # create a raw socket
+if os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+sniffer.bind((host, 0))
+
+# we want the IP headers included in the capture
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+# if using  windows, we need to send an IOCTL
+# to set up promisc mode on the network interface
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+# read a single packet
+print sniffer.recvfrom(65565)
+
+# if windows, turn promisc off
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
